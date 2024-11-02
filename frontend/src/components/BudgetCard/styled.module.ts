ba912e2edd9658fb511f30cf523b3bc2f9ef4288
JoviOakz <joao.mateussdc@gmail.com>
2024-11-02 16:33:11 -0300

    fix: BudgetCard border

--- frontend/src/components/BudgetCard/styled.module.ts
+++ frontend/src/components/BudgetCard/styled.module.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const CardWrapper = styled.div`
     width: 70%;
     height: 60%;
-    border: 1px solid black;
+    border: 3px solid green;
     border-radius: 15px;
     display: flex;
     justify-content: center;
